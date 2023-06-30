571527d4fd3039cde137c9fb6558b6be347c9e51
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-06-29 23:32:08 -0700

    Stripe✅

--- components/cardProducts.js
+++ components/cardProducts.js
@@ -1,10 +1,15 @@
-export default function CardProducts({ image, name, price }) {
+export default function CardProducts(props) {
+
+    const { product } = props;
+    const { id: price_id, unit_amount: cost, product: productInfo } = product;
+    const { name, description } = productInfo;
+
     return (
         <div className="max-w-xs bg-white shadow-md w-full rounded-lg overflow-hidden m-5">
-            <img src={image} alt={name} className="w-full h-40 object-cover" />
+            <img src={productInfo.images[0]} alt={name} className="w-full h-40 object-cover" />
             <div className="p-4">
                 <h3 className="text-gray-900 font-semibold text-lg">{name}</h3>
-                <p className="mt-2 text-gray-600">${price}</p>
+                <p className="mt-2 text-gray-600">${cost / 100}</p>
                 <button
                     // onClick={handleAddToCart}
                     className="mt-4 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
--- components/deals.js
+++ components/deals.js
@@ -2,47 +2,30 @@
 import useStore from "@/app/(store)/store";
 import CardProducts from "./cardProducts";
 import { useEffect } from 'react'
+import Stripe from "stripe";
+import { loadStripe } from '@stripe/stripe-js';
 
-export default function Deals() {
-    const items = useStore((state) => state.items)
-    const addItem = useStore((state) => state.addItems)
+async function getStripeProducts() {
+    const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+        apiVersion: "2022-11-15",
+    });
+    const res = await stripe.prices.list({
+        expand: ["data.product"],
+    });
+    const prices = res.data;
+    return prices;
+}
 
-    useEffect(() => {
-        const fetchDataAndSave = async () => {
-            const response = await fetch('https://api.stripe.com/sk_test_51NFnLOIrs3QRYXjf3wHraqWYWfg7QZCOJJeaubivqf7k4BXJ85lbWnK4JtwWEEtZjfgYbGPdcqqBGhw6GOo0dHjh00ZAjR6NnK/v1/products/')
-            const item = await response.json()
-            addItem(item)
-            console.log(item)
-            console.log(items.length)
-        }
-        fetchDataAndSave()
-    }, [addItem])
-    const products = [
-        {
-            image: "path/to/product-image-1.jpg",
-            name: "Product 1",
-            price: "19.99",
-        },
-        {
-            image: "path/to/product-image-2.jpg",
-            name: "Product 2",
-            price: "29.99",
-        },
-        {
-            image: "path/to/product-image-3.jpg",
-            name: "Product 3",
-            price: "39.99",
-        }
-    ];
+export default async function Deals() {
+    const products = await getStripeProducts();
+    console.log(products);
 
     return (
         <div className="flex flex-wrap justify-center gap-4">
             {products.map((product, index) => (
                 <CardProducts
                     key={index}
-                    image={product.image}
-                    name={product.name}
-                    price={product.price}
+                    product={product}
                 />
             ))}
         </div>
