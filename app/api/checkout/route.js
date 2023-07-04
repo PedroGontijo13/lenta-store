f619c40158282a99497748bd502fd9f8ddc54a27
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-07-03 19:28:29 -0700

    OK

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -14,7 +14,6 @@ export const POST = async (request) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
       apiVersion: "2022-11-15",
     });
-    
 
     const session = await stripe.checkout.sessions.create({
       success_url: "http://localhost:3000/sucess",
